ff0846c4934afded24b7b78d7c2e3aadbb122167
thang <leviethoangthang1234@gmail.com>
2024-08-24 18:48:19 +0700

    fetch track audio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,16 @@
 import AppHeader from "@/components/header/app.header";
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
-export default  async function HomePage() {
-  const res= await fetch(`https://spotify81.p.rapidapi.com/albums?ids=3IBcauSj5M2A6lTeffJzdv`,{
-      method:"GET",
-     
-      headers: {
-        "Content-Type": "application/json",
-        "x-rapidapi-host": "spotify81.p.rapidapi.com" ,
-       "x-rapidapi-key": "4e6b452c2amshdbaead8b668b88ap19b299jsn5ffe05e01ab3"
-      },
-  })
-  
-  const data = await res.json()
-  console.log(data)
+export default async function HomePage() {
 
   return (
 
-  <div>
-    <Container>
-    <MainSlider></MainSlider>
-    <MainSlider></MainSlider>
-    <MainSlider></MainSlider>
-    </Container>
+    <div>
+      <Container>
+        <MainSlider></MainSlider>
+        <MainSlider></MainSlider>
+        <MainSlider></MainSlider>
+      </Container>
     </div>
   );
 }
--- src/components/footer/main.footer.tsx
+++ src/components/footer/main.footer.tsx
@@ -1,39 +1,39 @@
 "use client"
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-import { Box,Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useHasMounted } from '@/app/utils/cutomhook';
 import { useEffect } from 'react';
 import test from 'node:test';
-const MainFooter=(props:any)=>{
-  
+const MainFooter = (props: any) => {
 
 
+  console.log(" check ", process.env.NEXT_PUBLIC_BACKEND_URL)
 
 
-    const hasMouted=useHasMounted(  );
-    if(!hasMouted)return(<></>)
-    return(
-   <Container sx={{display:"flex",backgroundColor:"#FFF",gap:'50px',boxShadow:"0 0 3px 0 rgba(0, 0, 0, 0.2)",position:"relative"}}>
-        <AudioPlayer  
-        style={{boxShadow:"none"}}  
-        // autoPlay
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/293&amp"
-        onPlay={e => console.log("onPlay")}   
+  const hasMouted = useHasMounted();
+  if (!hasMouted) return (<></>)
+  return (
+    <Container sx={{ display: "flex", backgroundColor: "#FFF", gap: '50px', boxShadow: "0 0 3px 0 rgba(0, 0, 0, 0.2)", position: "relative" }}>
+      <AudioPlayer
+        style={{ boxShadow: "none" }}
+
+        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}upload/TrackAudio/1724498742351-VuaHanVuaYeu-TrungTu-15121915.mp3`}
+        onPlay={e => console.log("onPlay")}
 
       />
       <div>
-      <div style={{color:"grey"}}>Eric</div>
-        <div><pre>Who Am I ?</pre></div> 
-        <span onClick={()=>props.setShowFooter(false)}><CloseIcon sx={{position:"absolute",top:"0",right:"0"}} ></CloseIcon></span>
+        <div style={{ color: "grey" }}>Eric</div>
+        <div><pre>Who Am I ?</pre></div>
+        <span onClick={() => props.setShowFooter(false)}><CloseIcon sx={{ position: "absolute", top: "0", right: "0" }} ></CloseIcon></span>
       </div>
-      
 
-   </Container>
 
- 
-    
-    )
+    </Container>
+
+
+
+  )
 }
 export default MainFooter
